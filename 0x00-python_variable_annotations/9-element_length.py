3ff945fc78acf69c50540ed3313fb19283316a6d
Ahmad <ahmad.new.m.v@gmail.com>
2024-10-16 20:52:27 +0000

    Last mandatory task's solution

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -6,4 +6,5 @@ from typing import List, Sequence, Tuple
 
 
 def element_length(lst: List[Sequence]) -> List[Tuple[Sequence, int]]:
+    """Returns a list of tuples of an item from sequence and its length"""
     return [(i, len(i)) for i in lst]
