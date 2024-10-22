24dde0a6803b6a7c1fb9a40b4eaa830b7a431e2c
Ahmad <ahmad.new.m.v@gmail.com>
2024-10-22 15:19:29 +0000

    Removed a bug

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -10,7 +10,7 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(max_delay: int, n: int) -> List[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """Returns a list of the delays in ascending order without using 'sort'"""
     res = []
     for i in asyncio.as_completed([wait_random(max_delay) for n in range(n)]):
