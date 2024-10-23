78537405773d835105b7403075127db667578bbc
Ahmad <ahmad.new.m.v@gmail.com>
2024-10-23 11:39:49 +0000

    Removed a bug

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -6,7 +6,8 @@ import asyncio
 import random
 from typing import AsyncGenerator
 
-async def async_generator() -> AsyncGenerator[float]:
+
+async def async_generator() -> AsyncGenerator[float, None]:
     """Yields 10 random values between 0 and 10 asyncrounously"""
     for i in range(10):
         yield random.uniform(0, 10)
