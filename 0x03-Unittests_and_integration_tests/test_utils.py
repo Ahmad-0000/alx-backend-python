5d86919d2bb231a9eafaeeb93f36ae7dd01f296d
Ahmad <ahmad.new.m.v@gmail.com>
2024-11-15 09:33:13 +0000

    New tests

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,6 +4,7 @@ Contains "utils" module tests
 """
 import unittest
 from utils import access_nested_map, get_json
+from utils import memoize as m
 from parameterized import parameterized
 from unittest.mock import Mock, patch
 
@@ -41,6 +42,32 @@ class TestGetJson(unittest.TestCase):
         m_version.get.use_count = m_version.get.call_count
         self.assertEqual(m_version.get.use_count, 1)
 
-        
+
+class TestMemoize(unittest.TestCase):
+    """Testing memoize funcionality"""
+
+    def test_memoize(self):
+        """Testing "memozie" function"""
+
+        class TestClass:
+            """Tmeporary class"""
+
+            def a_method(self):
+                """A method to be used indirectly by 'TestClass.a_property'"""
+                return 42
+
+            @m
+            def a_property(self):
+                """A method to be cached"""
+                return self.a_method()
+
+        with patch.object(TestClass, "a_method", return_value=42) as m_method:
+            obj = TestClass()
+            self.assertEqual(obj.a_property, 42)
+            self.assertEqual(obj.a_property, 42)
+            m_method.use_count = m_method.call_count
+            self.assertEqual(m_method.use_count, 1)
+
+
 if __name__ == "__main__":
     unittest.main()
